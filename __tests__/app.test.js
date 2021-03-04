d796e1628345e0a6fe2ff4f62cf2349b851a78e5
Maksim Saenko <mm.saenko@gmail.com>
2021-03-04 22:48:45 +0200

    configs

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,31 +1,103 @@
 import {
+  beforeEach,
   expect,
   describe,
   it,
 } from '@jest/globals';
-import Validator from '../src';
+import Validator from '../src/index';
 
-describe('test validator', () => {
-  const v = new Validator();
+describe('Validator', () => {
+  let v;
   let scheme;
 
-  beforeEach(() => {
-    scheme = v.string();
+  describe('Validation strings', () => {
+    beforeEach(() => {
+      v = new Validator();
+      scheme = v.string();
+    });
+
+    it('base', () => {
+      expect(scheme.isValid('')).toBeTruthy();
+    });
+
+    it('required', () => {
+      scheme.required();
+
+      expect(scheme.isValid('what does the fox say')).toBeTruthy();
+      expect(scheme.isValid('hexlet')).toBeTruthy();
+      expect(scheme.isValid(null)).toBeFalsy();
+      expect(scheme.isValid('')).toBeFalsy();
+
+      expect(scheme.contains('what').isValid('what does the fox say')).toBeTruthy();
+      expect(scheme.contains('whatthe').isValid('what does the fox say')).toBeFalsy();
+    });
+
+    it('min length', () => {
+      expect(scheme.minLength(0).isValid('')).toBeTruthy();
+      expect(scheme.minLength(2).isValid('')).toBeFalsy();
+      expect(scheme.minLength(2).isValid('abc')).toBeTruthy();
+    });
   });
 
-  it('validation', () => {
-    expect(scheme.isValid('')).toBeTruthy();
+  describe('Validation numbers', () => {
+    beforeEach(() => {
+      v = new Validator();
+      scheme = v.number();
+    });
+
+    it('base', () => {
+      expect(scheme.isValid(42)).toBeTruthy();
+      expect(scheme.isValid(null)).toBeTruthy();
+    });
+
+    it('required', () => {
+      scheme.required();
+
+      expect(scheme.isValid(42)).toBeTruthy();
+      expect(scheme.isValid(null)).toBeFalsy();
+    });
+
+    it('positive', () => {
+      expect(scheme.positive().isValid(42)).toBeTruthy();
+      expect(scheme.positive().isValid(0)).toBeTruthy();
+      expect(scheme.positive().isValid(-42)).toBeFalsy();
+    });
+
+    it('range', () => {
+      scheme.range(-5, 5);
+
+      expect(scheme.isValid(3)).toBeTruthy();
+      expect(scheme.isValid(-2)).toBeTruthy();
+      expect(scheme.isValid(42)).toBeFalsy();
+      expect(scheme.isValid(-42)).toBeFalsy();
+    });
   });
 
-  it('scheme required', () => {
-    scheme.required();
+  describe('Validation array', () => {
+    beforeEach(() => {
+      v = new Validator();
+      scheme = v.array();
+    });
 
-    expect(scheme.isValid('what does the fox say')).toBeTruthy();
-    expect(scheme.isValid('hexlet')).toBeTruthy();
-    expect(scheme.isValid(null)).toBeFalsy();
-    expect(scheme.isValid('')).toBeFalsy();
+    it('base', () => {
+      expect(scheme.isValid([])).toBeTruthy();
+      expect(scheme.isValid(null)).toBeTruthy();
+    });
 
-    expect(scheme.contains('what').isValid('what does the fox say')).toBeTruthy();
-    expect(scheme.contains('whatthe'.isValid('what does the fox say'))).toBeFalsy();
+    it('required', () => {
+      scheme.required();
+
+      expect(scheme.isValid([])).toBeTruthy();
+      expect(scheme.isValid(['jopa'])).toBeTruthy();
+      expect(scheme.isValid(null)).toBeFalsy();
+    });
+
+    it('sizeof', () => {
+      scheme.sizeof(2);
+
+      expect(scheme.isValid([1, 2])).toBeTruthy();
+      expect(scheme.isValid([1])).toBeFalsy();
+      expect(scheme.isValid([])).toBeFalsy();
+    });
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+/* eslint-disable */
+
+import StringScheme from './StringScheme';
+import NumberScheme from './NumberScheme';
+import ArrayScheme from './ArrayScheme';
+
 export default class Validator {
-  string() {}
-  isValid(str) {}
-  required() {}
-  contains(str) {}
+  string() {
+    return new StringScheme();
+  }
+
+  number() {
+    return new NumberScheme();
+  }
+
+  array() {
+    return new ArrayScheme();
+  }
 }
